13dca7cd19b8cec5c581cd11b256636d90c04b83
Alexandru Cretu <cretu.alexandru2000@gmail.com>
2023-03-10 12:58:27 +0200

    Use debounce

--- src/components/PokeSearchBar/PokeSearchBar.jsx
+++ src/components/PokeSearchBar/PokeSearchBar.jsx
@@ -1,21 +1,24 @@
-import React, { useState } from 'react';
-import './PokeSearchBar.css';
-import { useNavigate } from 'react-router-dom';
-import PokeReset from '../../assets/restart_icon.png';
+import React, { useCallback, useState } from "react";
+import "./PokeSearchBar.css";
+import { useNavigate } from "react-router-dom";
+import PokeReset from "../../assets/restart_icon.png";
+import { debounce } from "lodash";
 
 export default function PokeSearchBar({ onSearch }) {
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
   const navigator = useNavigate();
 
   const handleChange = (event) => {
     setSearchTerm(event.target.value);
-    onSearch(event.target.value);
+    onSearchDebounced(event.target.value);
   };
 
-  // const onResetClick = () => {
-  //   setSearchTerm('');
-  //   navigator('/');
-  // };
+  const onSearchDebounced = useCallback(
+    debounce((searchTerm) => {
+      onSearch(searchTerm);
+    }, 500),
+    []
+  );
 
   return (
     <div className="PokeSearchBar">
